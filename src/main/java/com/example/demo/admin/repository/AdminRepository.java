5d2c67ca1a5ad9f2b7d9426d5aab5c88e9546935
junil07 <wnsdlf199707@gmail.com>
2024-05-20 12:25:29 +0900

    Merge pull request #1 from junil07/qkrtmdtjq
    test

--- src/main/java/com/example/demo/admin/repository/AdminRepository.java
+++ src/main/java/com/example/demo/admin/repository/AdminRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.admin.repository;
 
-import com.example.project.admin.Entity.Admin;
+import com.example.demo.admin.Entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
