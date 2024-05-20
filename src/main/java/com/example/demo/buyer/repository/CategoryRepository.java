d7bf12365962ce346c2dd121a1ed202ef01a6d36
chaseongjin1 <162531702+chaseongjin1@users.noreply.github.com>
2024-05-20 11:34:13 +0900

    Delete src/main directory

--- src/main/java/com/example/demo/buyer/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.buyer.repository;
-
-import com.example.demo.buyer.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
